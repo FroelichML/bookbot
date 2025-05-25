dab00854c92fa1085ee2f468393becb28eb0c765
FroelichML <mfroelich@southern.edu>
2025-05-25 15:43:35 -0400

    update main and stats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import letter_count
 
 def get_book_text (path_to_file):
 	with open(path_to_file) as f:
@@ -9,5 +10,6 @@ def main():
 	book_text = get_book_text("books/frankenstein.txt")
 	book_wordcount = word_count(book_text)
 	print(f"{book_wordcount} words found in the document")
-
+	letter_dict = letter_count(book_text)
+	print (letter_dict)
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,16 @@ def word_count (text_to_split):
 	for i in range (0,len(book_list)):
 		counter += 1
 	return counter
+
+def letter_count (book_string):
+	lowercase_text = book_string.lower()
+	text_dict = {}
+
+	for letter in lowercase_text:
+		
+		if letter in text_dict:
+			text_dict[letter.lower()] = text_dict[letter.lower()] + 1
+		else:
+			text_dict[letter.lower()] = 1
+	
+	print (text_dict)
